1e8cf9729955ce808728439e95401252b2f29d4b
PulPee <konrad96pawlak@gmail.com>
2018-11-04 12:35:24 +0100

    change names in admin panel, update todo

--- ecommerce/ProductManager/models.py
+++ ecommerce/ProductManager/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     description = models.TextField(max_length=1000, null=True, blank=True)
     upperCategory = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=1000)# short description of product
@@ -13,11 +16,17 @@ class Product(models.Model):
     price = models.FloatField()
     avaialableAmt = models.FloatField()
     body = models.TextField(max_length=5000)# actual text appearing on product detail site
+    
+    def __str__(self):
+        return self.name
 
 class Image(models.Model):
     img = models.ImageField()
     description = models.CharField(max_length=100)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.__str__() + ' img'
 
 admin.site.register(Category)
 admin.site.register(Product)
