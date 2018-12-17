88f7222187b131c7666e7c5933d631873ab53a40
PulPee <konrad96pawlak@gmail.com>
2018-12-17 19:04:24 +0100

    fix: login form not showing on every subpage

--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-from .forms import SigninForm
 
 def mainPage(request):
     if request.method == 'GET':
-        loginForm = SigninForm()
-        return render(request, 'ecommerce/home.html', { 'loginForm': loginForm })
+        return render(request, 'ecommerce/home.html')
     elif request.method == 'POST':
         return HttpResponse('POST sent to main page')
